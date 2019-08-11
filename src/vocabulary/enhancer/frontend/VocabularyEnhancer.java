1ae9a3b453e1d42dba2fb83b5fe521259dec3e87
Spencer <spencercarr3@yahoo.com>
2019-08-10 19:54:47 -0700

    Fixed UI to be clean and fully functional.

--- src/vocabulary/enhancer/frontend/VocabularyEnhancer.java
+++ src/vocabulary/enhancer/frontend/VocabularyEnhancer.java
@@ -1,45 +1,72 @@
 package vocabulary.enhancer.frontend;
 
-import javax.swing.*;   //TODO: Fix this
-
 import vocabulary.enhancer.csv.CSVReaderUtil;
 import vocabulary.enhancer.data.WordDefinitionPair;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Color;
+
 import java.util.ArrayList;
 import java.util.List;
 
-//TODO: Set up score counter on UI and clean up UI
 public class VocabularyEnhancer extends JFrame {
     private JLabel definitionDisplay;
     private List<JButton> answerChoices;
-    private JPanel answersDisplay, answerChoicesDisplay;
+    private JButton scoreboard;
+    private JPanel displayPanel, definitionDisplayPanel, answerChoicesDisplayPanel, scoreDisplayPanel;
     private String currentCorrectAnswer;
-    private int score;
+    private int score, roundsPlayed;
 
     public VocabularyEnhancer() {
         definitionDisplay = new JLabel();
         answerChoices = new ArrayList<>(VocabularyEnhancerConstants.NUMBER_OF_ANSWER_CHOICES);
-        answersDisplay = new JPanel();
-        answerChoicesDisplay = new JPanel();
+
+        scoreboard = new JButton();
+
+        displayPanel = new JPanel();
+        definitionDisplayPanel = new JPanel();
+        answerChoicesDisplayPanel = new JPanel();
+        scoreDisplayPanel = new JPanel();
+
         score = 0;
 
         setUp();
     }
 
     private void setUp() {
+        displayPanel.setLayout(new BoxLayout(displayPanel, BoxLayout.Y_AXIS));
+
         setUpButtons();
         setUpNextRound();
         setUpDisplay();
+        setUpScoreDisplay();
+
+        displayPanel.add(definitionDisplayPanel);
+        displayPanel.add(answerChoicesDisplayPanel);
+        displayPanel.add(scoreDisplayPanel);
+    }
+
+    private void setUpScoreDisplay() {
+        scoreDisplayPanel.setLayout(new GridLayout(
+            VocabularyEnhancerConstants.NUMBER_OF_ROWS_FOR_SCORE_LAYOUT, VocabularyEnhancerConstants.NUMBER_OF_COLUMNS_FOR_SCORE_LAYOUT));
+
+        scoreboard.setText(getScoreDisplay());
+        scoreboard.setOpaque(true);
+
+        scoreDisplayPanel.add(scoreboard);
     }
 
     private void setUpDisplay() {
         setVisible(true);
         setResizable(true);
         setSize(VocabularyEnhancerConstants.JFRAME_DIMENSION);
-        add(answersDisplay);
+        add(displayPanel);
     }
 
     private void setUpNextRound() {
@@ -50,28 +77,26 @@ public class VocabularyEnhancer extends JFrame {
 
         currentCorrectAnswer = word;
         definitionDisplay.setText(definition);
+        definitionDisplayPanel.add(definitionDisplay);
 
         setUpAnswerChoices(nextWordAndDefinition);
     }
 
     private void setUpAnswerChoices(WordDefinitionPair nextWordAndDefinition) {
-        List<String> answerChoices = CSVReaderUtil.getAnswerChoices(nextWordAndDefinition);
+        answerChoicesDisplayPanel.setLayout(new GridLayout(
+            VocabularyEnhancerConstants.NUMBER_OF_ROWS_FOR_BUTTON_LAYOUT, VocabularyEnhancerConstants.NUMBER_OF_COLUMNS_FOR_BUTTON_LAYOUT));
 
-        //answersDisplay.add(definitionDisplay);
+        List<String> answerChoices = CSVReaderUtil.getAnswerChoices(nextWordAndDefinition);
 
         int choice = 0;
         for(String answerChoice : answerChoices) {
             this.answerChoices.get(choice).setText(answerChoice);
-            answersDisplay.add(this.answerChoices.get(choice));
+            answerChoicesDisplayPanel.add(this.answerChoices.get(choice));
             ++choice;
         }
     }
 
     private void setUpButtons() {
-//        GridLayout buttonLayout = new GridLayout(
-//            VocabularyEnhancerConstants.NUMBER_OF_ROWS_FOR_BUTTON_LAYOUT, VocabularyEnhancerConstants.NUMBER_OF_COLUMNS_FOR_BUTTON_LAYOUT);
-        answersDisplay.setLayout(new BoxLayout(answersDisplay, BoxLayout.Y_AXIS));
-
         for(int button = 0; button < VocabularyEnhancerConstants.NUMBER_OF_ANSWER_CHOICES; ++button) {
             this.answerChoices.add(new JButton());
             this.answerChoices.get(button).addActionListener(e -> checkAnswer(((JButton) e.getSource()).getText()));
@@ -81,10 +106,29 @@ public class VocabularyEnhancer extends JFrame {
     private void checkAnswer(String chosenAnswer) {
         boolean theCorrectAnswerWasChosen = chosenAnswer.equals(currentCorrectAnswer);
 
+        ++roundsPlayed;
+
         if(theCorrectAnswerWasChosen) {
             ++score;
+            scoreboard.setBackground(Color.GREEN);
+        }else {
+            scoreboard.setBackground(Color.RED);
         }
 
+        scoreboard.setText(getScoreDisplay());
         setUpNextRound();
+    }
+
+    private String getScoreDisplay() {
+        boolean atLeastOneRoundHasBeenPlayed = (roundsPlayed != 0);
+
+        double scorePercentage =
+            ( (atLeastOneRoundHasBeenPlayed) ? (((double)score) / roundsPlayed * 100) : (0.00) );
+
+        String scoreDisplay =
+            (score + "/" + roundsPlayed)
+                + (" (" +VocabularyEnhancerConstants.decimalFormatter.format(scorePercentage) + "%)");
+
+        return scoreDisplay;
     }
 }
--- src/vocabulary/enhancer/frontend/VocabularyEnhancerConstants.java
+++ src/vocabulary/enhancer/frontend/VocabularyEnhancerConstants.java
@@ -1,10 +1,18 @@
 package vocabulary.enhancer.frontend;
 
 import java.awt.Dimension;
+import java.text.DecimalFormat;
 
 public class VocabularyEnhancerConstants {
-    public final static int NUMBER_OF_ANSWER_CHOICES = 8;
+
     public final static Dimension JFRAME_DIMENSION = new Dimension(500, 500);
+
+    public final static DecimalFormat decimalFormatter = new DecimalFormat("0.00");
+
+    public final static int NUMBER_OF_ANSWER_CHOICES = 8;
     public final static int NUMBER_OF_ROWS_FOR_BUTTON_LAYOUT = 2;
     public final static int NUMBER_OF_COLUMNS_FOR_BUTTON_LAYOUT = 4;
+    public final static int NUMBER_OF_ROWS_FOR_SCORE_LAYOUT = 1;
+    public final static int NUMBER_OF_COLUMNS_FOR_SCORE_LAYOUT = 1;
+
 }
